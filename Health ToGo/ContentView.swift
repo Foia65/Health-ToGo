93f6c126fffc2a93f4b68452c3aaf147bb5ceada
Francesco Foianesi <foiasoft@gmail.com>
2025-07-17 18:11:16 +0200

    Initial Commit

---
+++ Health ToGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Health ToGo
+//
+//  Created by Francesco Foianesi on 17/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Health ToGo/Health_ToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_ToGoApp.swift
+//  Health ToGo
+//
+//  Created by Francesco Foianesi on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Health_ToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
